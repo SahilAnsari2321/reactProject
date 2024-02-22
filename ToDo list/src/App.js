4e39ea2f471a2a35135fdd29d5a03202aead933d
Mohammad Sahil <112580325+SahilAnsari2321@users.noreply.github.com>
2024-02-22 23:32:31 +0530

    Update App.js

--- ToDo list/src/App.js
+++ ToDo list/src/App.js
@@ -10,7 +10,6 @@ function App() {
   const addTodo = (todo) => {
     setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
   };
-
   const updateTodo = (id, todo) => {
     setTodos((prev) =>
       prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
